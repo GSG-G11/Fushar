c8ff0518056100848e21890171fc75f6df8e1fff
unknown <montherismail90@gmail.com>
2022-04-12 19:52:18 +0300

    create nav component Relates #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './Components/Nav';
 
 function App() {
-  return <div>hello</div>;
+  return <div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
+  </div>;
 }
 
 export default App;
