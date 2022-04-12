a79958a3262ef5eb26de55521b94c91bbedc4ef8
unknown <montherismail90@gmail.com>
2022-04-12 22:08:16 +0300

    style navbar component Relates #11

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './Components/Nav';
+import Nav from './Components/navbar/Nav';
 
 function App() {
   return <div>
     <Router>
       <Nav />
+      <hr />
       <Routes>
         <Route/>
       </Routes>
